fefea5b2ef9d0ce4f11809ebfeac1751ad90eb13
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-10-08 17:50:03 -0400

    create dynamic div to show movies

--- script.js
+++ script.js
@@ -9,6 +9,25 @@ async function getMovies(){
     const respData = await resp.json();
 
     console.log(respData);
+
+    respData.results.forEach(movie =>{
+        const { poster_path, title, vote_average } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML =`
+        
+            <img 
+            src="${IMGPATH + poster_path}" 
+            alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span> ${vote_average}</span>
+            </div>
+       
+        `;
+        document.body.appendChild(movieEl);
+    })
+
     // respData.results.forEach((movie)=>{
     //     const img = document.createElement("img");
     //     img.src= IMGPATH + movie.poster_path;
